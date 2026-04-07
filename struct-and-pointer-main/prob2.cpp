48efad598c77625b75d0c1e6c607414e01ef8a0f
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-07 12:02:13 +0800

    Update prob2.cpp

---
+++ struct-and-pointer-main/prob2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <cmath>
+#include "circle.h"
+
+
+int main() {
+    int cx, cy, r, px, py;
+
+    cx = 0;
+    cy = 0;
+    r = 5;
+    px = 3;
+    py = 4;
+
+    int d = sqrt( pow(px, 2) + pow(py, 2) );
+    int rr = pow(r, 2);
+
+    
+}
